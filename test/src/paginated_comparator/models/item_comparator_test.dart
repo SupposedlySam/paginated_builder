f33805ddaeb962ab961995f3279a0c4c26975ee6
Jonah Walker <supposedlysam@gmail.com>
2022-10-17 22:33:54 -0600

    test: item_comparator

---
+++ test/src/paginated_comparator/models/item_comparator_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:paginated_builder/paginated_builder.dart';
+
+void main() {
+  test('should compare by value', () {
+    expect(
+      const ItemComparator<int>(
+        previous: 1,
+        current: 2,
+        next: 3,
+        isFirstItem: true,
+        isLastItem: true,
+      ),
+      const ItemComparator<int>(
+        previous: 1,
+        current: 2,
+        next: 3,
+        isFirstItem: true,
+        isLastItem: true,
+      ),
+    );
+  });
+}
