4f9e4f6dcf9ba4217d8f6717564c8600f92abaa5
Jonah Walker <supposedlysam@gmail.com>
2023-02-10 10:02:12 -0500

    refactor: remove examples directory in favor of examples_ext

--- examples/flutter_infinite_list/lib/app.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_infinite_list/posts/posts.dart';
-
-class App extends MaterialApp {
-  const App({super.key}) : super(home: const PostsPage());
-}
